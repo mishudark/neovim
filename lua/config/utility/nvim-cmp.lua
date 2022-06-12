3568d328408f35f324b29172a03a38c0e5ec5aac
mishudark <mishu.drk@gmail.com>
2022-06-12 21:04:19 +0200

    feat: improve cmp config

--- lua/config/utility/nvim-cmp.lua
+++ lua/config/utility/nvim-cmp.lua
@@ -1,77 +1,121 @@
--- nvim-cmp configuration
-local api = vim.api
-local fn = vim.fn
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 local has_words_before = function()
-    local line, col = unpack(api.nvim_win_get_cursor(0))
-    return col ~= 0 and api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end
-    },
-
-    mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        }),
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif has_words_before() and luasnip.expand_or_jumpable() then
-                api.nvim_feedkeys(
-                api.nvim_replace_termcodes(
-                '<Plug>luasnip-expand-or-jump',
-                true, true, true
-                ), '', true
-                )
-            else
-                fallback()
-            end
-        end,
-
-        ['<S-Tab>'] = function()
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                api.nvim_feedkeys(
-                api.nvim_replace_termcodes(
-                '<Plug>luasnip-jump-prev',
-                true, true, true
-                ), '', true
-                )
-            end
-        end
-    },
-
-    sources = {
-        { name = 'path' },
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lua' },
-        { name = 'luasnip' },
-    },
-
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.menu = ({
-                path = "[Path]",
-                nvim_lsp = "[LSP]",
-                nvim_lua = "[Lua]",
-                luasnip = "[LuaSnip]",
-            })[entry.source.name]
-            vim_item.kind = vim.lsp.protocol.CompletionItemKind[vim_item.kind]
-
-            return vim_item
-        end
-    }
+local default_cmp_opts = {
+  enabled = function()
+    -- disable completion in comments
+    local context = require('cmp.config.context')
+    -- keep command mode completion enabled when cursor is in a comment
+    if vim.api.nvim_get_mode().mode == 'c' then
+      return true
+    else
+      return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
+    end
+  end,
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    -- disabled for autopairs mapping
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {
+      'i',
+      's',
+    }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {
+      'i',
+      's',
+    }),
+  }),
+  window = {
+  },
+  experimental = {
+    ghost_text = true,
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+  formatting = {
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.menu = ({
+        nvim_lsp = '[lsp]',
+        luasnip = '[snip]',
+        buffer = '[buf]',
+        path = '[path]',
+        nvim_lua = '[nvim_api]',
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
 }
+
+cmp.setup(default_cmp_opts)
+
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  }),
+})
+
+cmp.setup.filetype('TelescopePrompt', {
+  enabled = false,
+})
+
+-- cmp.setup.cmdline(':', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({
+--     { name = 'path' },
+--   }, {
+--     { name = 'cmdline' },
+--   }),
+-- })
