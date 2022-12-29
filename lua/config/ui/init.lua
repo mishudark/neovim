e1a469c39a1d315156a18ef45283a2ff60647bbd
mishudark <mishudark@iMac-de-Nelyda.local>
2022-12-29 15:33:33 -0800

    fix: rename onedarkpro -> onedark

--- lua/config/ui/init.lua
+++ lua/config/ui/init.lua
@@ -5,7 +5,7 @@ vim.g.vscode_italic_comment = 1
 vim.g.vscode_disable_nvimtree_bg = false
 
 -- Set colorscheme
-vim.cmd('colorscheme onedarkpro')
+vim.cmd('colorscheme onedark')
 
 -- Load UI modules
 require('config.ui.feline')
